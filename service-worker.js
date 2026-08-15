d534af36fddb2cbe06ebc8b835315231cd731262
lorecabre <lorecabre87@gmail.com>
2026-08-15 12:02:07 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE = 'entrenamiento-v1';
+const FILES = ['./','./entrenamiento.html','./manifest.webmanifest','./icono.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
