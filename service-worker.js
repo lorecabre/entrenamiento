51e376496c75dbe38deb587aab0571a03aa87b08
lorecabre <lorecabre87@gmail.com>
2026-08-15 21:35:19 -0300

    Actualiza cache de la app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'entrenamiento-v1';
+const CACHE = 'entrenamiento-v2';
 const FILES = ['./','./entrenamiento.html','./manifest.webmanifest','./icono.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
